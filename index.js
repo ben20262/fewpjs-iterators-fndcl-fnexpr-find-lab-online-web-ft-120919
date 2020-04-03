c63cd7ff1d2c0831eabd648cfbe8ab53241d6dea
ben20262 <ben20262@aim.com>
2020-04-03 13:59:23 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(superArray) {
+  let win = superArray.find(function(w) {
+    return w.result === 'W'
+  })
+  return !!win ? win.year : win
+}
